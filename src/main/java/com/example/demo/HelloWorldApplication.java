22e61fde915716aeb98a54f509f9657cce20bb54
nakashima2toshio <nakashima2toshio@gmail.com>
2018-01-03 17:14:39 +0900

    second commit

--- src/main/java/com/example/demo/HelloWorldApplication.java
+++ src/main/java/com/example/demo/HelloWorldApplication.java
@@ -1,11 +1,19 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@SpringBootApplication
+@Controller
+@EnableAutoConfiguration
 public class HelloWorldApplication {
-
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return "home Hello World responsebody";
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
