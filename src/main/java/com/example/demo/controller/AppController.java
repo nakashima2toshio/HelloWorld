1f29d8f8e1343d5caf63f8797058f6f5ddaf9962
nakashima2toshio <nakashima2toshio@gmail.com>
2018-01-03 17:26:36 +0900

    com 4

--- src/main/java/com/example/demo/controller/AppController.java
+++ src/main/java/com/example/demo/controller/AppController.java
@@ -3,11 +3,11 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
+@RequestMapping("/home")
 public class AppController {
-    @RequestMapping("/home")
 
     public String home() {
-        return "home Hello World!";
+        return "index";
     }
 
 }
